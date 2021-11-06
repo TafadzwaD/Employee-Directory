abb4f2f3f999d1b83cc17847b20bda008ddd6764
TafadzwaD <tafadzwademba@gmail.com>
2021-11-06 14:34:19 +0200

    App bar of EmployeeList Screen

---
+++ lib/views/employeeList/employee_list_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class EmployeeListScreen extends StatefulWidget {
+  const EmployeeListScreen({Key? key}) : super(key: key);
+
+  @override
+  _EmployeeListScreenState createState() => _EmployeeListScreenState();
+}
+
+class _EmployeeListScreenState extends State<EmployeeListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+          ),
+        ),
+        title: const Text('Employee Directory',style: TextStyle(fontSize: 12),),
+      ),
+    );
+  }
+}
