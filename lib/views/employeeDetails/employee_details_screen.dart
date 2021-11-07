d9687e8155e6a28c269fcc4025f29e5c62ff1546
TafadzwaD <tafadzwademba@gmail.com>
2021-11-07 18:33:59 +0200

    Stack used to design the dotted appearance of the employee profile card

--- lib/views/employeeDetails/employee_details_screen.dart
+++ lib/views/employeeDetails/employee_details_screen.dart
@@ -1,4 +1,5 @@
 import 'package:employee/models/employee_model.dart';
+import 'package:employee/views/employeeList/widgets/dashed_line_widget.dart';
 import 'package:flutter/material.dart';
 
 class EmployeeDetailsScreen extends StatelessWidget {
@@ -13,19 +14,89 @@ class EmployeeDetailsScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(Icons.arrow_back_sharp),
-        ),actions: [
-          Text('yEST')
-      ],
-      ),
-      body: Column(
-        children: const <Widget>[
-
+        backgroundColor: Colors.white,
+        leading: Transform.scale(
+          scale: 1.3,
+          child: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back_sharp,
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        actions: [
+          Transform.scale(
+            scale: 1.3,
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert_sharp,
+                color: Colors.black54,
+              ),
+            ),
+          ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15.0,right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 200,
+              child: Stack(
+                children: const [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: SizedBox(
+                      height: 200,
+                      child: RotatedBox(
+                        quarterTurns: 1,
+                        child: DashedLine(),),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: SizedBox(
+                      height: 200,
+                      child: RotatedBox(
+                        quarterTurns: 1,
+                        child: DashedLine(),),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomLeft,
+                    child: DashedLine(),
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: DashedLine(),
+                  ),
+
+                ],
+              ),
+            ),
+            // Container(
+            //   width: double.infinity,
+            // height: 500,
+            // child: Column(
+            //   children: [
+            //     Container(
+            //       height: 300,
+            //       child: RotatedBox(
+            //         quarterTurns: 1,
+            //         child: DashedLine(),),
+            //     ),
+            //     SizedBox(height: 20,),
+            //     DashedLine(),
+            //
+            //   ],
+            // )),
+          ],
+        ),
       ),
     );
   }
