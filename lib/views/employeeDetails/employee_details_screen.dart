cc84c9ea14aff9b42f5f644998f12c4e4ca2baeb
TafadzwaD <tafadzwademba@gmail.com>
2021-11-07 21:44:59 +0200

    Refactored Icon Container on employee details screen to be a standalone widget to avoid code repetition

--- lib/views/employeeDetails/employee_details_screen.dart
+++ lib/views/employeeDetails/employee_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:employee/models/employee_model.dart';
 import 'package:employee/views/employeeDetails/widgets/employee_avatar_widget.dart';
+import 'package:employee/views/employeeDetails/widgets/icon_widget.dart';
 import 'package:employee/views/employeeDetails/widgets/vdivider_widget.dart';
 import 'package:employee/views/employeeList/widgets/dashed_line_widget.dart';
 import 'package:flutter/material.dart';
@@ -100,77 +101,39 @@ class EmployeeDetailsScreen extends StatelessWidget {
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: Row(
-                            children: [
-                              Container(
-                                width: 45,
-                                height: 45,
-                                decoration: const BoxDecoration(
-                                    shape: BoxShape.rectangle,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(17)),
-                                    gradient: LinearGradient(
-                                      begin: Alignment.topRight,
-                                      end: Alignment.bottomLeft,
-                                      colors: [
-                                        Colors.teal,
-                                        Colors.yellow,
-                                      ],
-                                    )),
-                                child: const Center(
-                                  child: Icon(
-                                    Icons.phone,
-                                    color: Colors.black38,
-                                  ),
-                                ),
-                              ),
-                              // const VerticalDivider(width: 30,),
-                              const Vdivider(),
+                            children: const [
 
-                              Container(
-                                width: 45,
-                                height: 45,
-                                decoration: const BoxDecoration(
-                                    shape: BoxShape.rectangle,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(17)),
-                                    gradient: LinearGradient(
-                                      begin: Alignment.centerRight,
-                                      end: Alignment.bottomLeft,
-                                      colors: [
-                                        Colors.teal,
-                                        Colors.yellow,
-                                      ],
-                                    )),
-                                child: const Center(
-                                  child: Icon(
-                                    Icons.phone_android,
-                                    color: Colors.black38,
-                                  ),
-                                ),
-                              ),
-                              const Vdivider(),
-                              Container(
-                                width: 45,
-                                height: 45,
-                                decoration: const BoxDecoration(
-                                    shape: BoxShape.rectangle,
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(17)),
-                                    gradient: LinearGradient(
-                                      begin: Alignment.centerLeft,
-                                      end: Alignment.topRight,
-                                      colors: [
-                                        Colors.teal,
-                                        Colors.yellow,
-                                      ],
-                                    )),
-                                child: const Center(
-                                  child: Icon(
-                                    Icons.email,
-                                    color: Colors.black38,
-                                  ),
-                                ),
-                              ),
+                              IconContainer(icon: Icons.phone,gradient: LinearGradient(
+                                begin: Alignment.topRight,
+                                end: Alignment.bottomLeft,
+                                colors: [
+                                  Colors.teal,
+                                  Colors.yellow,
+                                ],
+                              ),),
+                              // const VerticalDivider(width: 30,),
+                              Vdivider(),
+
+                              IconContainer(icon: Icons.phone_android, gradient: LinearGradient(
+                                begin: Alignment.centerRight,
+                                end: Alignment.bottomLeft,
+                                colors: [
+                                  Colors.teal,
+                                  Colors.yellow,
+                                ],
+                              ),)
+,
+
+                              Vdivider(),
+                              IconContainer(icon: Icons.email, gradient: LinearGradient(
+                                begin: Alignment.centerLeft,
+                                end: Alignment.topRight,
+                                colors: [
+                                  Colors.teal,
+                                  Colors.yellow,
+                                ],
+                              ),),
+
                             ],
                           ),
                         ),
--- lib/views/employeeDetails/widgets/icon_widget.dart
+++ lib/views/employeeDetails/widgets/icon_widget.dart
@@ -2,24 +2,18 @@ import 'package:flutter/material.dart';
 
 class IconContainer extends StatelessWidget {
   final IconData icon;
-  const IconContainer({Key? key, required this.icon}) : super(key: key);
+  final LinearGradient gradient;
+  const IconContainer({Key? key, required this.icon,required this.gradient}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 45,
       height: 45,
-      decoration: const BoxDecoration(
+      decoration:  BoxDecoration(
           shape: BoxShape.rectangle,
-          borderRadius: BorderRadius.all(Radius.circular(17)),
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Colors.teal,
-              Colors.yellow,
-            ],
-          )),
+          borderRadius: const BorderRadius.all(Radius.circular(17)),
+          gradient: gradient),
       child:  Center(
         child: Icon(
           icon,
