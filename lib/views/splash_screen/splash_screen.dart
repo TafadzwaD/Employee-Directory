d6a014b637b43031a3ce9f6da63f2697323fac97
TafadzwaD <tafadzwademba@gmail.com>
2021-11-09 22:56:47 +0200

    Added splash screen with 3 seconds delay.. Resolves #7

---
+++ lib/views/splash_screen/splash_screen.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:employee/views/employeeList/employee_list_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+      () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+            builder: (BuildContext context) => const EmployeeListScreen()),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset('assets/launcherIcon/employeeDirectory.png'),
+      ),
+    );
+  }
+}
